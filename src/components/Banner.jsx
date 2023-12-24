cde4653f5f279cff2a0ce46eb9983a4a8758d6d0
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-24 22:35:07 +0600

    add framer motion

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+import { motion } from "framer-motion";
 export default function Banner() {
   return (
     <div className="max-w-6xl mx-auto p-6 my-12 flex flex-col md:flex-col lg:flex-row gap-6 items-center justify-center">
-      <div className="w-1/2 text-center space-y-6">
+      <motion.div
+        animate={{ x: [50, 50, 50], opacity: 1, scale: 1 }}
+        transition={{
+          duration: 5,
+          delay: 0.3,
+          ease: [0.5, 0.71, 1, 1.5],
+        }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        whileHover={{ scale: 1.2 }}
+        className="w-1/2 text-center space-y-6"
+      >
         <h1 className="font-bold text-4xl sm:text-6xl space-y-4 ">
           Lets share Our <br />
           <span className="text-orange-600 font-extrabold">Knowledge</span>
@@ -11,7 +22,7 @@ export default function Banner() {
         <p className="text-slate-600 font-semibold text-2xl ">
           together we will take care our knowledge
         </p>
-      </div>
+      </motion.div>
       <div className="w-full sm:w-1/2 mx-auto ">
         <img
           className="rounded-lg p-4"
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 export default function Service({ service }) {
   console.log(service);
   return (
-    <div className="w-4/5 mx-auto shadow-lg p-2  my-6 rounded-lg flex  flex-col ">
+    <motion.div
+      className="w-4/5 mx-auto shadow-lg p-2  my-6 rounded-lg flex  flex-col "
+      animate={{ x: [50, 50, 50], opacity: 1, scale: 1 }}
+      transition={{
+        duration: 5,
+        delay: 0.3,
+        ease: [0.5, 0.71, 1, 1.5],
+      }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      whileHover={{ scale: 1.2 }}
+    >
       <div className="w-full  flex-1 ">
         <img src={service.image} alt="image" />
       </div>
@@ -13,6 +23,6 @@ export default function Service({ service }) {
         </h1>
         <p className="text-slate-500 font-serif">{service.short_description}</p>
       </div>
-    </div>
+    </motion.div>
   );
 }
