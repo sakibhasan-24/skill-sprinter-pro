5e4e7a905fab5aadde3e3800a4c4862a8b8cf34d
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-24 21:43:03 +0600

    footer done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
